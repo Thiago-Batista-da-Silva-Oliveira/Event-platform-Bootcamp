5eac592e943fc41d86943c6fd56dbef7aadcb359
Thiago <gmoliveira69@gmail.com>
2022-06-27 10:06:32 -0300

    building sidebar

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,13 +1,19 @@
 import { gql, useQuery } from "@apollo/client"
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
+import { Video } from "./components/Video"
 import { Event } from "./pages/Event"
 
 function App() {
 
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
         <Header />
-    </>
+        <main className="flex flex-1">
+           <Video />
+           <Sidebar />
+        </main>
+    </div>
   )
 }
 
--- event-platform/src/components/Video.tsx
+++ event-platform/src/components/Video.tsx
@@ -2,7 +2,8 @@
 
 export const Video = () => {
     return (
-        <>
-        </>
+        <div className="flex-1">
+             Video
+        </div>
     )
 }
